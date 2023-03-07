e9ac1b34e25d6c1e0cc83029dcf31f28414acc09
yang.wang <yang.wang@ushow.media>
2023-03-07 20:37:04 +0800

    提交sockjs、以及STOMP相关内容

--- src/main/java/com/example/spring/websocket/WebSocketConfig.java
+++ src/main/java/com/example/spring/websocket/WebSocketConfig.java
@@ -7,7 +7,10 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
-
+/**
+ * websocket server与client还可以协商子协议，例如：STOMP（Simple Text Oriented Message Protocol）
+ * 参考地址：https://docs.spring.io/spring-framework/docs/6.0.6/reference/html/web.html#websocket-stomp
+ */
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
@@ -19,5 +22,11 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(myHandler, "/myHandler").addInterceptors(new HttpSessionHandshakeInterceptor());
     }
+//    使用sockjs方式建立websocket连接，参考地址：https://docs.spring.io/spring-framework/docs/6.0.6/reference/html/web.html#websocket
+//    sockjs jsclient https://github.com/sockjs/sockjs-client/
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+//        registry.addHandler(myHandler, "/myHandler").withSockJS();
+//    }
 
 }
